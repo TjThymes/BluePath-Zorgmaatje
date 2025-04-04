5d1c520d1bfef787a6aa5302086dd35e2fcce41e
TjThymes <thijmenroozendaal1@gmail.com>
2025-04-04 16:45:44 +0200

    #

--- BluePathBackend/BluePathBackend/Objects/RouteStep.cs
+++ BluePathBackend/BluePathBackend/Objects/RouteStep.cs
@@ -6,10 +6,10 @@ namespace BluePath_Backend.Objects
     {
         public Guid Id { get; set; }
         public string RouteType { get; set; } // "A" of "B"
-        public int StepOrder { get; set; }
+        public int? StepOrder { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string IconName { get; set; } // bv. "appointment", "hospital", "recovery"
+        public string? IconName { get; set; } // bv. "appointment", "hospital", "recovery"
         public float x { get; set; } // X Location
         public float y { get; set; } // Y Location
     }
